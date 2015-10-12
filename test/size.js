86fb215d2beeeb93faf0aa8dee001227b222d16b
190n <benjamin.j.grant@gmail.com>
2015-10-12 16:05:30 -0700

    Start rewrite

--- test/size.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert'),
-    five = require('../dist/five.min.js').five;
-
-suite('five.size', function() {
-    test('diagonal algorithm', function() {
-        assert(new five.Size(4, 3).diagonal == 5);
-    });
-});
